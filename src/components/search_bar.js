63ef04ce1a56f2d8d2d33b0efa04c532836848a8
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-22 16:58:32 +0200

    Changed functional to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,16 +4,33 @@
 // So when the user types something in the input,
 // we need to make an api request to the YouTube API
 
-import React from 'react';
+// import React and get the property Component as a variable named Component
+import React, { Component } from 'react';
 // We need to make sure that we include React in all of our components where we use JSX
 // because, when transpiling JSX to javascript, 'return <input />' turns into a call, like:
 // React.createElement. So it will throw an error if React is not in the scope of the file
 
-const SearchBar = () => {
+// const SearchBar = () => {
+//
+//   return <input />;
+//
+// }; // This is a functional react component, because, it literally is a function
 
-  return <input />;
+// The following component is class component
+// meaning: define class SearchBar and give it access to
+// all functionality that React.component has
+class SearchBar extends Component {
+  // every class based React component that we create,
+  // must have a defined 'render' metod
 
-};  // this will generate an html input element
+  render() {
+    // in our render method, we must return some JSX, otherwise will throw an error
+    return <input />;
+
+  }
+
+}
+
 
 //make out SearchBar available for other files to access it
 export default SearchBar;
