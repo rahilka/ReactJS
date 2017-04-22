83fdbdd62dd8b83268fe7886379dd5782a7c7870
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-22 15:14:24 +0200

    Render our App component

--- src/index.js
+++ src/index.js
@@ -20,16 +20,23 @@
 import React from 'react';
 //meaning: go and find the library 'react' in my modules
 // and assign it to the variable React
+// React is used to create and manage our components
 
 import ReactDOM from 'react-dom';
 // to render a component to the DOM, we don't use the react core library
 // we use ReactDOM
+// ReactDOM is used to interact with the actual DOM
 
 // Create a new component that should produce some HTML
 
 // render = place this component's html on the page
 
-const App = function() {
+// When we create a component,
+// we create a class, a type of a component
+
+// es6 feature: declaring a function
+
+const App = () => {
 
   return <div>HI!</div>; // this html-like is JSX
   // JSX = subset of JS that allows us to write,
@@ -45,12 +52,24 @@ const App = function() {
 
 }
 
+// App is class, not an instance!
+// So before we try to render the component to the DOM,
+// we need to create an instance from the class
+
 // es6 feature: const
 // declaring a variable with const means that
 // this is the final value that this varaible is going to have
 // it is constant!
 
+
 // Take this component's generated HTML and
 // put in on the page (in the DOM)
 
-ReactDOM.render(App);
+// ReactDOM.render(App); //this is wrong cos in this way we pass the class 'App'
+// and we need to pass the App instance,
+// which we do with wrapping the class name into JSX tags
+
+ReactDOM.render(<App />, document.querySelector('.container'));
+// the second argument is reference to an existing DOM node on the page
+// meaning : find the element on the page with class of 'container', and then
+// try to render the App component into that div
