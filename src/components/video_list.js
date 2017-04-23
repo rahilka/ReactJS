adf62b7a5bffcb9a9764e99430aef1e5d06688d8
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-23 13:20:44 +0200

    Added video list item component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,16 +4,23 @@
 // it doesn't rerender itself, so we can make it functional component
 
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // when using a functional component, the 'props' object will come as an argument to the function
 // in class somponent, the props are available anywhere, in any method we define, with: 'this.props'
 const VideoList = (props) => {
   // like : const videos = props.videos
+  // to iterate over the videos array wll use 'map'
+
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video = {video} />
+  });
+
   return (
 
     <ul className="col-md-4 list-group" >
 
-      {props.videos.length}
+      {videoItems}
 
     </ul>
 
---
+++ src/components/video_list_item.js
@@ -0,0 +1,11 @@
+// Component for each video from the list item
+
+import React from 'react';
+
+const VideoListItem = (props) => {
+
+  return <li>Video</li>
+
+};
+
+export default VideoListItem;
