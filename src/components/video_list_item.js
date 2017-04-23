6de7e49b1e7c92b64bfbf6c766ae7814e2187c3e
Rahilka Simonova <rahilka.simonova@yahoo.com>
2017-04-23 13:40:48 +0200

    Added image and header to the video lists

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,9 +2,38 @@
 
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  // get the video that we passed to VideoList as a property
+  // es6 syntax: instead of pass 'props' argument and then const video = props.video;
+  // just pass '{video}'
 
-  return <li>Video</li>
+  // !! Put '{}' inside the JSX when referencing a javascript variable
+
+  const imageUrl = video.snippet.thumbnails.default.url;
+
+  return (
+
+    <li className="list-group-item">
+
+        <div className="video-list media">
+
+            <div className="media-left">
+
+              <img className="media-object" src={imageUrl} />
+
+            </div>
+
+            <div className="media-body">
+
+              <div className="media-heading">{video.snippet.title}</div>
+
+            </div>
+
+        </div>
+
+    </li>
+
+  )
 
 };
 
